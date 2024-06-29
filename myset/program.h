aacd4506327db571ba90a0d5a72bfda47bfaad57
talfig <talfig8@gmail.com>
2024-06-29 19:38:00 +0300

    Update program.h
    adding names to the prototypes

--- myset/program.h
+++ myset/program.h
@@ -21,6 +21,6 @@
  * @param E Pointer to the fifth set.
  * @param F Pointer to the sixth set.
  */
-void boot_program(set *, set *, set *, set *, set *, set *);
+void boot_program(set *A, set *B, set *C, set *D, set *E, set *F);
 
 #endif /* PROGRAM_H */
